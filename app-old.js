b94b9e60e8c368f200b87d14fa3f1ef60886a5cc
Ivan <ivancabeza85@gmail.com>
2020-02-27 23:15:40 +0100

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-type': 'application/json' });
+
+        let salida = {
+            nombre: 'Ivan',
+            edad: 34,
+            url: req.url
+        }
+
+        res.write(JSON.stringify(salida));
+        // res.write('Hola mundo');
+        res.end();
+
+    })
+    .listen(8080);
+
+console.log('Escuchando el puerto 8080');
